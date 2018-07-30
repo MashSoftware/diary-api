cbdce6b90b1237f55dd3d2f86f2a0402d83bb990
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-07-30 23:27:17 +0100

    setup flake8 & fixes

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'U2Kl7#oE3CbCxwQ1Fxqt1Ukt3Ha%243&'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
