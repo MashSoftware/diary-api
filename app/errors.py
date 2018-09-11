49506f83a469d704e1e6dd8c9fdd85784d517f2c
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-09-11 22:22:07 +0100

    more error handlers

--- app/errors.py
+++ app/errors.py
@@ -3,11 +3,46 @@ from flask import Response
 from app import app
 
 
+@app.errorhandler(400)
+def bad_request(error):
+    return Response(response='{"message":"Bad request"}',
+                    mimetype='application/json',
+                    status=400)
+
+
 @app.errorhandler(404)
 def not_found(error):
     return Response(response='{"message":"Not found"}',
                     mimetype='application/json',
                     status=404)
+
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return Response(response='{"message":"Method not allowed"}',
+                    mimetype='application/json',
+                    status=405)
+
+
+@app.errorhandler(406)
+def not_acceptable(error):
+    return Response(response='{"message":"Not acceptable"}',
+                    mimetype='application/json',
+                    status=406)
+
+
+@app.errorhandler(415)
+def unsupported_media_type(error):
+    return Response(response='{"message":"Unsupported media type"}',
+                    mimetype='application/json',
+                    status=415)
+
+
+@app.errorhandler(429)
+def too_many_requests(error):
+    return Response(response='{"message":"Too many requests"}',
+                    mimetype='application/json',
+                    status=429)
 
 
 @app.errorhandler(500)
