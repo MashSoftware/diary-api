accc107c118aa0e2a43bbed72b8e86d259b0adad
Matt Shaw <matthew.shaw@digital.landregistry.gov.uk>
2018-09-22 14:30:32 +0100

    child resource routes

--- app/views/child.py
+++ app/views/child.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 from flask import Blueprint, Response, request
 from flask_negotiate import consumes, produces
@@ -6,8 +7,28 @@ from werkzeug.exceptions import BadRequest
 
 from app import db
 from app.models import Child, User
+from jsonschema import FormatChecker, ValidationError, validate
 
 child = Blueprint('child', __name__)
+
+# JSON schema for child requests
+with open('openapi.json') as json_file:
+    openapi = json.load(json_file)
+child_schema = openapi["components"]["schemas"]["ChildRequest"]
+
+
+@child.route("/children", methods=['GET'])
+@produces('application/json')
+def get_children():
+    """Get Children."""
+    children = Child.query.order_by(Child.created_at).all()
+    result = []
+    for child in children:
+        result.append(child.as_dict())
+
+    return Response(response=json.dumps(result, sort_keys=True, separators=(',', ':')),
+                    mimetype='application/json',
+                    status=200)
 
 
 @child.route("/children", methods=['POST'])
@@ -17,6 +38,12 @@ def create_child():
     """Create a new Child."""
     child_request = request.json
 
+    # Validate request against schema
+    try:
+        validate(child_request, child_schema, format_checker=FormatChecker())
+    except ValidationError as e:
+        raise BadRequest(e.message)
+
     # Create a new child object
     child = Child(
         first_name=child_request["first_name"],
@@ -24,6 +51,7 @@ def create_child():
         date_of_birth=child_request["date_of_birth"]
     )
 
+    # Add user to child
     users = []
     for user_id in child_request["users"]:
         user = User.query.get(str(user_id))
@@ -44,26 +72,66 @@ def create_child():
     return response
 
 
-@child.route("/children", methods=['GET'])
-@produces('application/json')
-def get_children():
-    """Get Children."""
-    children = Child.query.order_by(Child.created_at).all()
-    result = []
-    for child in children:
-        result.append(child.as_dict())
-
-    return Response(response=json.dumps(result, sort_keys=True, separators=(',', ':')),
-                    mimetype='application/json',
-                    status=200)
-
-
 @child.route("/children/<uuid:id>", methods=['GET'])
 @produces('application/json')
 def get_child(id):
-    """Get a Child for a given ID."""
+    """Get a Child for a given id."""
     child = Child.query.get_or_404(str(id))
 
     return Response(response=repr(child),
                     mimetype='application/json',
                     status=200)
+
+
+@child.route("/children/<uuid:id>", methods=['PUT'])
+@consumes("application/json")
+@produces('application/json')
+def update_child(id):
+    """Update a Child for a given id."""
+    child_request = request.json
+
+    # Validate request against schema
+    try:
+        validate(child_request, child_schema, format_checker=FormatChecker())
+    except ValidationError as e:
+        raise BadRequest(e.message)
+
+    # Retrieve existing child
+    child = Child.query.get_or_404(str(id))
+
+    # Update child
+    child.first_name = child_request["first_name"].title(),
+    child.last_name = child_request["last_name"].title(),
+    child.date_of_birth = child_request["date_of_birth"],
+    child.updated_at = datetime.utcnow()
+
+    # Add user to child
+    users = []
+    for user_id in child_request["users"]:
+        user = User.query.get(str(user_id))
+        if user:
+            users.append(user)
+        else:
+            raise BadRequest()
+    child.users = users
+
+    # Commit child to db
+    db.session.add(child)
+    db.session.commit()
+
+    return Response(response=repr(child),
+                    mimetype='application/json',
+                    status=200)
+
+
+@child.route("/children/<uuid:id>", methods=['DELETE'])
+@produces('application/json')
+def delete_child(id):
+    """Delete a Child for a given id."""
+    child = Child.query.get_or_404(str(id))
+
+    db.session.delete(child)
+    db.session.commit()
+    return Response(response=None,
+                    mimetype='application/json',
+                    status=204)
